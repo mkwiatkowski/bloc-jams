5edd30a85cf80b7eeeef508dbeee13169911086f
Judith Eekhout <jheekhout@hotmail.com>
2015-06-16 08:27:41 +0000

    added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -275,6 +275,7 @@ if (document.URL.match(/\/album.html/)) {
 require('./landing');
 require('./collection');
 require('./album');
+require('./profile');
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -377,6 +378,30 @@ $(document).ready(function() {
     $(this).fadeOut('slow');
   });
 });
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+// holds the name of our tab button container for selection later in the function
+var tabsContainer = ".user-profile-tabs-container"
+
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $(".tab-pane").addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+};
+
+ if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 });
 
 ;
